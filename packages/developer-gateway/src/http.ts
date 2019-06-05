18fb3737f5fff996a4403188184036fb136c723b
David Yan <david.yan@oasislabs.com>
2019-06-05 13:54:56 -0700

    Generate uuid and pass it as session key

--- packages/developer-gateway/src/http.ts
+++ packages/developer-gateway/src/http.ts
@@ -1,5 +1,17 @@
 import axios from 'axios';
+import _uuid from 'uuid';
 
+let uuid: any = undefined;
+
+// Browser.
+/* tslint:disable */
+if (typeof window !== 'undefined') {
+  uuid = _uuid.v4;
+}
+// Node.
+else {
+  uuid = require('uuid/v4');
+}
 /**
  * Http interface for making http requests to the developer gateway.
  */
@@ -8,14 +20,21 @@ export interface Http {
 }
 
 export class HttpRequest implements Http {
-  public constructor(public url: string) {}
+  /**
+   * session key passed to the developer gateway in the header.
+   */
+  private sessionKey: string;
+
+  public constructor(public url: string) {
+    this.sessionKey = uuid();
+  }
 
   public async post(api: string, body: Object): Promise<any> {
     const uri = `${this.url}/${api}`;
     let response = await axios.post(uri, body, {
       headers: {
         'X-OASIS-INSECURE-AUTH': 'example',
-        'X-OASIS-SESSION-KEY': 'example-session',
+        'X-OASIS-SESSION-KEY': this.sessionKey,
         'Content-type': 'application/json'
       }
     });
