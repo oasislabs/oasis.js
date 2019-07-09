6e8bfcccb5f473ef8f531de36a0bae26bb47d083
Armani Ferrante <armaniferrante@gmail.com>
2019-07-08 17:37:57 -0700

    Inflate the deflated mantle-interface wasm section

--- packages/service/rollup/rollup.config.browser.ts
+++ packages/service/rollup/rollup.config.browser.ts
@@ -38,7 +38,8 @@ export default {
     commonjs({
       namedExports: {
         '../../node_modules/js-sha3/src/sha3.js': [ 'keccak256' ],
-        '../../node_modules/eventemitter3/index.js': [ 'EventEmitter' ]
+        '../../node_modules/eventemitter3/index.js': [ 'EventEmitter' ],
+        '../../node_modules/pako/index.js': [ 'inflateRaw' ]
       }
     }),
     globals(),
