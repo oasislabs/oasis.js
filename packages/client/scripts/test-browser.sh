81721b73b3bd5a092ccb408e62ac2dc2d29bf1a7
Armani Ferrante <armaniferrante@gmail.com>
2019-08-29 14:12:31 -0700

    Ensure build fails on browser test failure

--- packages/client/scripts/test-browser.sh
+++ packages/client/scripts/test-browser.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eo pipefail
+
 # Source common for cleanup on exit.
 source scripts/common.sh
 
@@ -9,6 +11,3 @@ python3 -m http.server 8000 &
 # Run the browser tests.
 node test/browser/service/test.js
 node test/browser/confidential/test.js
-# Do not run the e2e test in CI as it requires
-# an external network.
-#node test/browser/e2e/test.js
