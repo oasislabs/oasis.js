c22e2e919b8f6a3c2e4ef72e627d2864ea060733
Nick Hynes <nhynes@oasislabs.com>
2019-11-07 10:34:07 -0800

    Only populate workspace with valid services (#327)

--- packages/client/src/workspace.ts
+++ packages/client/src/workspace.ts
@@ -57,9 +57,13 @@ export default new Proxy(
         find
           .fileSync(/target\/service\/.*\.wasm/, projectRoot)
           .reduce((services: any, wasmPath: string) => {
-            const bytecode = fs.readFileSync(wasmPath);
-            const idl = fromWasmSync(bytecode);
-            services[idl.name] = new ServiceDefinition(bytecode, idl);
+            try {
+              const bytecode = fs.readFileSync(wasmPath);
+              const idl = fromWasmSync(bytecode);
+              services[idl.name] = new ServiceDefinition(bytecode, idl);
+            } catch {
+              // ignore invalid service
+            }
             return services;
           }, workspaceCache);
 
